7e4ba8c01e67890b9111f67fed27f76e05f18276
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2016-02-10 13:07:54 -0200

    Age calculator

---
+++ js/calculator_age.js
@@ -0,0 +1,11 @@
+function getAge(dateOfBirthday) {
+    var today = new Date();
+    var birthDate = new Date(dateOfBirthday);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+
+    return age;
+}
